5ece3dfcbe336cc8e8c41d8421ba85148450318b
Daniel Fletcher <daniel.j.fletcher2@gmail.com>
2017-02-20 12:04:35 -0800

    Write out tests for User model

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  describe "GET #new" do
+    it "renders the new users page"
+  end
+
+  describe "POST #create" do
+
+    context "with valid credentials" do
+      it "saves the new user to the database"
+      it "redirects to user's goals page"
+    end
+
+    context "with invalid credentials" do
+      it "renders the new users template"
+      it "displays errors on the sign up page"
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  subject(:sally) { User.create!(username: "sally", password: "test123" ) }
+
   describe "validations" do
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:password_digest) }
     it { should validate_presence_of(:session_token) }
     it { should validate_uniqueness_of(:username) }
     it { should validate_uniqueness_of(:session_token) }
+    it { should validate_length_of(:password).is_at_least(6) }
   end
 
   describe "associations" do
@@ -14,16 +17,32 @@ RSpec.describe User, type: :model do
 
   describe "class methods" do
     describe "::generate_session_token" do
-      it "generates a url safe random 16 character string"
+      it "generates a url safe random 16 character string" do
+        token1 = User.generate_session_token
+        token2 = User.generate_session_token
+
+        expect(token1).to be_a(String)
+        expect(token1.size).to eq(16)
+        expect(token1).not_to eq(token2)
+      end
     end
 
     describe "::find_by_credentials" do
+
       context "when the user is in the database" do
-        it "returns the user with the matching credentials"
+        it "returns the user with the matching credentials" do
+          expect(User.find_by_credentials(username: "sally", password: "test123"))
+            .to eq(sally)
+        end
       end
 
       context "when the user is not in the database" do
-        it "returns nil"
+        it "returns nil" do
+          expect(User.find_by_credentials(username: "hsdkhfgk", password: "test123"))
+            .to be_nil
+          expect(User.find_by_credentials(username: "sally", password: "dgadgdgg"))
+            .to be_nil
+        end
       end
     end
 
@@ -31,18 +50,36 @@ RSpec.describe User, type: :model do
 
   describe "instance methods" do
     describe "#reset_session_token!" do
-      it "changes the users session token"
+      it "changes the users session token" do
+        token = sally.session_token
+        sally.reset_session_token!
+
+        expect(token).not_to eq(sally.session_token)
+      end
     end
 
     describe "#password=" do
-      it "creates a password digest using BCrypt and saves it into the database"
+      it "creates a password digest using BCrypt" do
+        expect(:password=).to receive(BCrypt::Password.create)
+        sally.password = "new_password"
+      end
 
-      it "does not save the password"
+      it "saves the password digest into the database" do
+        expect(sally.password_digest).to eq(BCrypt::Password.create("test123"))
+      end
+
+      it "does not save the password into the database" do
+        expect(sally.password).to be_nil
+      end
     end
 
     describe "#is_password?" do
-      it "uses BCrypt to validate the password"
-    end
+      it "uses BCrypt to validate the password" do
+        expect(:is_password?).to receive(BCrypt::Password.is_password?)
 
+        expect(sally.is_password?("test123")).to be_true
+        expect(sally.is_password?("new_password")).to be_false
+      end
+    end
   end
 end
