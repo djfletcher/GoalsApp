aa040d267cf4f6713406ca1f9b0028365e56c4a2
Daniel Fletcher <daniel.j.fletcher2@gmail.com>
2017-02-20 11:21:21 -0800

    Add specs for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-
-describe User do
-
   describe "validations" do
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:password_digest) }
@@ -14,22 +9,40 @@ describe User do
     it { should validate_uniqueness_of(:session_token) }
   end
 
-  describe "associations"
+  describe "associations" do
+  end
 
   describe "class methods" do
-    describe "::generate_session_token"
+    describe "::generate_session_token" do
+      it "generates a url safe random 16 character string"
+    end
 
-    describe "::find_by_credentials"
+    describe "::find_by_credentials" do
+      context "when the user is in the database" do
+        it "returns the user with the matching credentials"
+      end
+
+      context "when the user is not in the database" do
+        it "returns nil"
+      end
+    end
 
   end
 
   describe "instance methods" do
-    describe "#reset_session_token!"
+    describe "#reset_session_token!" do
+      it "changes the users session token"
+    end
 
-    describe "#password="
+    describe "#password=" do
+      it "creates a password digest using BCrypt and saves it into the database"
 
-    describe "#is_password?"
+      it "does not save the password"
+    end
+
+    describe "#is_password?" do
+      it "uses BCrypt to validate the password"
+    end
 
   end
-
 end
