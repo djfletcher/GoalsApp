10e430e1385481d85b75ff47f1d53535674c2135
Daniel Fletcher <daniel.j.fletcher2@gmail.com>
2017-02-20 15:37:32 -0800

    Finish code for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
@@ -17,30 +29,30 @@ RSpec.describe User, type: :model do
 
   describe "class methods" do
     describe "::generate_session_token" do
-      it "generates a url safe random 16 character string" do
+      it "generates a url safe random 22 character string" do
         token1 = User.generate_session_token
         token2 = User.generate_session_token
 
         expect(token1).to be_a(String)
-        expect(token1.size).to eq(16)
+        expect(token1.size).to eq(22)
         expect(token1).not_to eq(token2)
       end
     end
 
     describe "::find_by_credentials" do
-
       context "when the user is in the database" do
         it "returns the user with the matching credentials" do
-          expect(User.find_by_credentials(username: "sally", password: "test123"))
+          sally.save
+          expect(User.find_by_credentials("sally", "test123"))
             .to eq(sally)
         end
       end
 
       context "when the user is not in the database" do
         it "returns nil" do
-          expect(User.find_by_credentials(username: "hsdkhfgk", password: "test123"))
+          expect(User.find_by_credentials("hsdkhfgk", "test123"))
             .to be_nil
-          expect(User.find_by_credentials(username: "sally", password: "dgadgdgg"))
+          expect(User.find_by_credentials("sally", "dgadgdgg"))
             .to be_nil
         end
       end
@@ -60,25 +72,23 @@ RSpec.describe User, type: :model do
 
     describe "#password=" do
       it "creates a password digest using BCrypt" do
-        expect(:password=).to receive(BCrypt::Password.create)
-        sally.password = "new_password"
+        (BCrypt::Password).should_receive(:create).with("test123")
+        User.create(username: "bob", password: "test123")
       end
 
       it "saves the password digest into the database" do
-        expect(sally.password_digest).to eq(BCrypt::Password.create("test123"))
+        expect(User.find(sally.id).password_digest).to be_truthy
       end
 
       it "does not save the password into the database" do
-        expect(sally.password).to be_nil
+        expect(User.find(sally.id).password).to be_nil
       end
     end
 
     describe "#is_password?" do
-      it "uses BCrypt to validate the password" do
-        expect(:is_password?).to receive(BCrypt::Password.is_password?)
-
-        expect(sally.is_password?("test123")).to be_true
-        expect(sally.is_password?("new_password")).to be_false
+      it "returns true if the correct password is given" do
+        expect(sally.is_password?("test123")).to be true
+        expect(sally.is_password?("new_password")).to be false
       end
     end
   end
